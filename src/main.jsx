9e8fbc1317e1a9bfe7deadb945194dce40ce2cf3
Duke Lee <minhduc8a2.1@gmail.com>
2023-08-11 07:24:30 +0700

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
